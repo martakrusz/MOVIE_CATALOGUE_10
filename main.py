dd6805e8beffd8b201b73a52165352aa56b5e410
Marta Kruszewska <martakruszewska007@gmail.com>
2020-08-23 07:27:13 +0200

    test git

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask import request
+import tmdb_client as tc
+import os
 
 app=Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    movies=[]
+    movies= tc.get_popular_movies()
     return render_template("homepage.html", movies=movies)
-
 
 
 if __name__ == '__main__':
