6b429663ae3e311e4fa43c302f663428309d5d16
Łukasz Zosiak <lukasz.zosiak@gmail.com>
2020-09-24 10:44:21 +0200

    added missing mocking in test_get_single_movie_cast and test_get_movies_list_type_popular

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -13,7 +13,9 @@ def test_get_poster_url_uses_default_size():
    # Porównanie wyników
    assert expected_default_size in poster_url
 
-def test_get_movies_list_type_popular():
+def test_get_movies_list_type_popular(monkeypatch):
+   requests_mock = Mock()
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
    movies_list = tmdb_client.get_movies_list(list_type="popular")
    assert movies_list is not None
 
@@ -43,8 +45,11 @@ def test_get_single_movie(monkeypatch):
    assert movie == mock_single_movie
 
 
-def test_get_single_movie_cast():
+def test_get_single_movie_cast(monkeypatch):
    mock_movie_cast = ['cast1', 'cast2']
+   requests_mock = Mock()
+   requests_mock.return_value.json.return_value = {'cast': mock_movie_cast}
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
    cast = tmdb_client.get_single_movie_cast(100)
    assert type(cast) == type(mock_movie_cast)
 
